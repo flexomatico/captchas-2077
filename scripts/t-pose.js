84b24b27698e81fc257883aa17e2f59116b70dc3
Felix Möller <felixmoeller@Felixs-MacBook-Air.local>
2023-11-23 14:55:24 +0100

    Add angle check between arm and body. Not tested.

--- scripts/t-pose.js
+++ scripts/t-pose.js
@@ -2,7 +2,8 @@ let video;
 var w, h;
 let poseNet;
 let pose, skeleton;
-
+let evalJoints;
+let minConfidence = 0.5
 
 function setup(){
   w = window.innerWidth * 0.7;
@@ -13,7 +14,9 @@ function setup(){
   video.hide();
 
   poseNet = ml5.poseNet(video, modelLoaded);
-  poseNet.on('pose', gotPoses, {flipHorizontal: false});
+  poseNet.on('pose', gotPoses, {flipHorizontal: false, minConfidence: minConfidence});
+
+  angleMode(DEGREES);
 }
 
 function draw(){
@@ -21,7 +24,7 @@ function draw(){
 
   if (pose) {
     drawSkeleton();
-    evaluateTPose
+    evaluateTPose();
   }
 }
 
@@ -56,4 +59,44 @@ function drawSkeleton() {
     ellipse(a.position.x, a.position.y, 32);
     ellipse(b.position.x, b.position.y, 32);
   });
+}
+
+function resetEvalJoints(){
+  evalJoints = {
+    leftShoulder: null,
+    rightShoulder: null,
+    leftWrist: null,
+    rightWrist: null,
+    leftHip: null,
+    rightHip: null
+  }
+}
+
+function evaluateTPose() {
+  resetEvalJoints();
+
+  // get joint positions
+  evalJoints['leftShoulder'] = pose.leftHip.confidence > minConfidence ? createVector(pose.leftShoulder.x, pose.leftShoulder.y) : null;
+  evalJoints['rightShoulder'] = pose.leftHip.confidence > minConfidence ? createVector(pose.rightShoulder.x, pose.rightShoulder.y) : null;
+  evalJoints['leftWrist'] = pose.leftHip.confidence > minConfidence ? createVector(pose.leftWrist.x, pose.leftWrist.y) : null;
+  evalJoints['rightWrist'] = pose.leftHip.confidence > minConfidence ? createVector(pose.rightWrist.x, pose.rightWrist.y) : null;
+  evalJoints['leftHip'] = pose.leftHip.confidence > minConfidence ? createVector(pose.leftHip.x, pose.leftHip.y) : null;
+  evalJoints['rightHip'] = pose.leftHip.confidence > minConfidence ? createVector(pose.rightHip.x, pose.rightHip.y) : null;
+
+  // check if all joints are there, else stop
+  for (key in evalJoints) {
+    if (evalJoints[key] == null)
+      return;
+  }
+
+  let leftArmVec = evalJoints['leftWrist'].sub(evalJoints['leftShoulder']).normalize();
+  let leftBodyVec = evalJoints['leftHip'].sub(evalJoints['leftShoulder']).normalize();
+  let rightArmVec = evalJoints['rightWrist'].sub(evalJoints['rightShoulder']).normalize();
+  let rightBodyVec = evalJoints['rightHip'].sub(evalJoints['rightShoulder']).normalize();
+
+  let leftAngle = leftArmVec.angleBetween(leftBodyVec);
+  let rightAngle = rightArmVec.angleBetween(rightBodyVec);
+
+  console.log('leftAngle: ' + leftAngle);
+  console.log('rightAngle: ' + rightAngle);
 }
