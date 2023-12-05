2d85ef2a0317c544c62d23ba559ef4269c41f8d5
Felix Möller <felixmoeller@Felixs-MacBook-Air.local>
2023-12-05 11:02:59 +0100

    Add image capture to photo page.

---
+++ scripts/idPhoto.js
@@ -0,0 +1,35 @@
+let video;
+let w = 640;
+let h = 480;
+let isLiveView = true;
+
+function setup() {
+  let canvas = createCanvas(640, 480);
+  canvas.parent("camera");
+  video = createCapture(VIDEO);
+  video.size(640, 480);
+  video.hide();
+}
+
+function draw() {
+    if(isLiveView)
+        mirrorVideo();
+}
+
+function mirrorVideo() {
+  push()
+  // mirror video horizontally
+  translate(video.width, 0);
+  scale(-1, 1);
+  image(video, 0, 0);
+}
+
+function takePhoto() {
+    console.log("Take photo")
+    isLiveView = false;
+    setTimeout(advancePage,1000);
+}
+
+function advancePage() {
+    window.location.href = "./templateCaptcha.html";
+}
